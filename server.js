cf372781eb58ff1336e80403b02dc0e17ef2f3df
MohammadReza <mohammadreza.ala@snapp.cab>
2023-01-01 01:35:00 +0330

    feat: lots of stuff

--- server.js
+++ server.js
@@ -26,30 +26,43 @@ const s3Client = new AWS({
     region: "default",
     s3ForcePathStyle: true,
 });
-const CACHING_ENABLED = process.env.CACHING_ENABLED;
+const isProduction = process.env.NODE_ENV == 'production'
+const DEV_CACHE = isProduction;
+const OSS_CACHING_ENABLED = 0 && process.env.CACHING_ENABLED;
 const cache = Object.create(null);
 function getCache (key) {
-    return CACHING_ENABLED? cache[key]: false;
+    return OSS_CACHING_ENABLED? cache[key]: false;
 }
 function setCache (key, value) {
-    return CACHING_ENABLED? cache[key] = value: value;
+    return OSS_CACHING_ENABLED? cache[key] = value: value;
 }
 
+const template = (DEV_CACHE && cache.template) || (cache.template = fs.readFileSync(`./template.html`).toString());
+function htmlRenderer (rawHtml = '', fileSystemPath = '') {
+    return template.replace('<!-- %%%BODY%%% -->', rawHtml || fs.readFileSync(fileSystemPath).toString());
+};
+const editor = htmlRenderer(null, `./editor.html`);
+
+
 // ! -----------------------------------------
 // ! -----------------------------------------
 // ! -----------------------------------------
 
-app.get('/', (req, res) => {
-    res.sendFile('./index.html', {root: __dirname })
+app.get('/', async (req, res) => {
+    const pages = (await listObjects()).map(s3Obj => `<li style="">
+        <a href='${s3Obj.Key.replace('.html','')}' >${s3Obj.Key}</a>
+        <a href='${s3Obj.Key.replace('.html','')}/delete'>x</a>
+    </li>`).join('');
+    res.send(htmlRenderer( null, './home.html').replace('<!-- %%%PAGES%%% -->', pages))
 })
 
 app.get('/file', (req, res) => {
     const downloads = fs.readdirSync('/tmp').map(fileName => `<li>
         <a href='/download/${fileName}' download>${fileName}</a>
         <a href='/delete/${fileName}'> x</a>
-    </li>`);
-    const str = fs.readFileSync('./file.html').toString();
-    res.send(str.replace('&&&DOWNLOADS&&&', downloads))
+    </li>`).join('');
+    const filePage = htmlRenderer(null, './file.html');
+    res.send(filePage.replace('&&&DOWNLOADS&&&', downloads))
 })
 
 app.post('/upload', upload.single('file'), (req, res) => {
@@ -92,24 +105,28 @@ app.get('/reset', (req, res) => {
 })
 
 app.get('/editor', (req, res) => {
-    res.sendFile('./editor.html', {root: __dirname })
+    res.send(editor)
 })
 
-app.get('/pages/:slug', async (req, res) => {
-    const template = cache.template || (cache.template = fs.readFileSync(`./template.html`).toString());
-    const slug = req.params.slug;
+app.get(/\/(.+)$/, async (req, res) => {
+    let slug = req.params[0];
+    if(slug.endsWith('/')) slug = slug.slice(0, slug.length - 1)
     let body = await fileReader(slug); 
     if (body.ok) {
-        const page = template.replace('<!-- %%%BODY%%% -->', body.txt)
-        res.send(page);
+        res.send(htmlRenderer(body.txt));
     } else {
         res.status(400).send(body.txt)
     }
 })
 
-app.get('/pages/:slug/edit', async (req, res) => {
-    const editor = cache.editor || (cache.editor = fs.readFileSync(`./editor.html`).toString());
-    const slug = req.params.slug;
+app.get(/\/(.+)\/delete\/?$/, async (req, res) => {
+    const slug = req.params[0];
+    await deleteObject(`${slug}`); 
+    res.redirect('back');
+})
+
+app.get(/\/(.+)\/edit\/?$/, async (req, res) => {
+    const slug = req.params[0];
     const pageContent = await fileReader(slug);
     if (pageContent.ok) {
         const page = editor.replace(`/* initialData */`, `initialData: \`${pageContent.txt}\`,`)
@@ -119,18 +136,15 @@ app.get('/pages/:slug/edit', async (req, res) => {
     }
 })
 
-app.post('/pages/:slug/save', upload.none(), async (req, res) => {
-    const slug = req.params.slug;
+app.post(/\/(.+)\/save\/?$/, upload.none(), async (req, res) => {
+    const slug = req.params[0];
     await fileWriter(slug, req.body.content);
     res.send(`page: /${slug} saved`);
 })
 
-app.listen(port, () => {
-    console.log(`Handy app listening on port ${port}`)
-})
-
 async function fileReader (slug) {
-    const url = `https://${process.env.BUCKET_NAME}.storage.iran.liara.space/pages/${slug}.html`;
+    console.log({slug})
+    const url = `https://${process.env.BUCKET_NAME}.storage.iran.liara.space/${slug}`;
     const cacheResponse = getCache(slug);
     if (cacheResponse) {
         return { ok: true, txt: cacheResponse };
@@ -150,14 +164,31 @@ async function fileReader (slug) {
 }
 
 async function fileWriter (slug, content) {
-    const params = {
+    console.log({slug, content})
+    setCache(slug, null);
+    return s3Client.putObject({
         Body: content, 
         ACL: "public-read",
         CacheControl: 'max-age=600; public',
         ContentType: 'text/html',
         Bucket: process.env.BUCKET_NAME, 
-        Key: `pages/${slug}.html`,
-    };
-    setCache(slug, null);
-    return s3Client.putObject(params).promise();
+        Key: `${slug}`,
+    }).promise();
 }
+
+async function listObjects (slug, content) {
+    return (await s3Client.listObjects({
+        Bucket: process.env.BUCKET_NAME, 
+    }).promise()).Contents;
+}
+
+async function deleteObject (key) {
+    return s3Client.deleteObject({
+        Bucket: process.env.BUCKET_NAME, 
+        Key: key
+    }).promise();
+}
+
+app.listen(port, () => {
+    console.log(`Handy app listening on port ${port}`)
+})
